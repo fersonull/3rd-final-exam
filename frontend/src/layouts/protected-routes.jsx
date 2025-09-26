03e39d0f420243a645b3898271632f72f664e856
Casper <culaspero@gmail.com>
2025-09-26 16:14:17 +0800

    [ TO-FIX ] protected route and token handling

--- frontend/src/layouts/protected-routes.jsx
+++ frontend/src/layouts/protected-routes.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/auth-context";
+import { Outlet } from "react-router-dom";
 
 export default function ProtectedRoutes() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const { token } = useContext(AuthContext);
+
+  console.log(token);
+
+  if (token) {
+    <Outlet />;
+  }
+
+  return <div>Unauthorized.</div>;
 }
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,8 +1,10 @@
 import { lazy, Suspense } from "react";
 import PublicRoutes from "./layouts/public-routes";
 import AuthRoutes from "./layouts/auth-routes";
+import ProtectedRoutes from "./layouts/protected-routes";
 import Home from "./pages/home";
 import AuthPage from "./pages/auth/auth-page";
+import Index from "./pages/dashboard";
 
 export const routes = [
   {
@@ -20,6 +22,15 @@ export const routes = [
       {
         path: "/auth",
         element: <AuthPage />,
+      },
+    ],
+  },
+  {
+    element: <ProtectedRoutes />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Index />,
       },
     ],
   },
